fc69648e20b5c1be673aff9f30c4748996bdd522
DAFTDESAILLY <desita_gsheko@hotmail.com>
2023-08-02 22:40:24 -0600

    subiendo la carga de imagenes en tiempo real

--- app/src/main/java/com/desailly/chat_sensuadesires/Perfil/EditarImagenPerfil.kt
+++ app/src/main/java/com/desailly/chat_sensuadesires/Perfil/EditarImagenPerfil.kt
@@ -1,6 +1,7 @@
 package com.desailly.chat_sensuadesires.Perfil
 
 import android.app.Dialog
+import android.app.ProgressDialog
 import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
@@ -14,11 +15,18 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import com.desailly.chat_sensuadesires.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 private lateinit var ImagenPerfilActualizar : ImageView
 private  lateinit var BtnElegirImagen : Button
 private  lateinit var BtnActualizarImagen : Button
 private var imageUri : Uri?=null
+
+private lateinit var firebaseAuth: FirebaseAuth
+private lateinit var progressDialog: ProgressDialog
 
 class EditarImagenPerfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,15 +37,66 @@ class EditarImagenPerfil : AppCompatActivity() {
         BtnElegirImagen = findViewById(R.id.BtnElegirImagenDe)
         BtnActualizarImagen = findViewById(R.id.BtnActualizarImagen)
 
+        progressDialog = ProgressDialog(this@EditarImagenPerfil)
+        progressDialog.setTitle("espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+
         BtnElegirImagen.setOnClickListener {
         MostrarDialog()
         // Toast.makeText(applicationContext, "Seleccionar Imagen", Toast.LENGTH_SHORT).show()
+
         }
 
         BtnActualizarImagen.setOnClickListener {
-            Toast.makeText(applicationContext, "Actualizar Imagen", Toast.LENGTH_SHORT).show()
+        ValidarImagen()
+        // Toast.makeText(applicationContext, "Actualizar Imagen", Toast.LENGTH_SHORT).show()
         }
     }
+
+    private fun ValidarImagen(){
+        if(imageUri == null){
+            Toast.makeText(applicationContext, "Es necesario una imagen", Toast.LENGTH_SHORT).show()
+        }else{
+            SubirImagen()
+        }
+    }
+
+    private fun SubirImagen() {
+        progressDialog.setMessage("Actualizando Imagen")
+        progressDialog.show()
+        val rutaImagen = "Perfil_usuario/"+ firebaseAuth.uid
+        val referenceStore = FirebaseStorage.getInstance().getReference(rutaImagen)
+        referenceStore.putFile(imageUri!!).addOnSuccessListener {tarea->
+            val uriTarea : Task<Uri> = tarea.storage.downloadUrl
+            while (!uriTarea.isSuccessful);
+            val urlImagen = "${uriTarea.result}"
+            ActualizarImagenBD(urlImagen)
+
+        }.addOnFailureListener {e->
+            Toast.makeText(applicationContext, "No se ha podido subir la imagen debido a:${e.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun ActualizarImagenBD(urlImagen: String) {
+        progressDialog.setMessage("Actualizar imagen de perfil")
+        val hashMap : HashMap<String,Any> = HashMap()
+        if (imageUri != null){
+            hashMap["imagen"] = urlImagen
+        }
+
+        val reference = FirebaseDatabase.getInstance().getReference("Usuarios")
+        reference.child(firebaseAuth.uid!!).updateChildren(hashMap).addOnSuccessListener {
+            progressDialog.dismiss()
+            Toast.makeText(applicationContext, "Imagen Actualizada", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {e->
+            Toast.makeText(applicationContext, "No se ha actualizado tu imagen debido a ${e.message}", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
     private fun  AbrirGaleria(){
         val intent = Intent(Intent.ACTION_PICK)
         intent.type="image/*"
